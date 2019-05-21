88e4361e813aa206ca3c10a75cc963dbb23f55f2
Bryan Nelson <nelsonbm@coolsite.biz>
2019-05-20 23:21:04 -0700

    initial setup

---
+++ src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Calculator() {
+
+    return (
+        <div>
+            <h1>Calculator from calculator!</h1>
+        </div>
+    )
+}
+
+export default Calculator
