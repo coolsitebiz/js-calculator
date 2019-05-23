30dd2e7ec4483818fe3dfd9efd9418a6c86934a6
Bryan Nelson <nelsonbm@coolsite.biz>
2019-05-22 23:03:24 -0700

    styled buttons may delete later idk

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import Button from './Button'
+import '../App.css'
 
-function Calculator() {
+class Calculator extends React.Component { 
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="container">               
 
-    return (
-        <div>
-            <h1>Calculator from calculator!</h1>
-        </div>
-    )
+                <Button id="seven" text="7" btnClass="digit"/>
+                <Button id="eight" text="8" btnClass="digit"/>
+                <Button id="nine" text="9" btnClass="digit"/>
+                <Button id="divide" text="/" btnClass="operator"/>
+                <Button id="four" text="4" btnClass="digit"/>
+                <Button id="five" text="5" btnClass="digit"/>
+                <Button id="six" text="6" btnClass="digit"/>
+                <Button id="multiply" text="*" btnClass="operator"/>
+                <Button id="one" text="1" btnClass="digit"/>
+                <Button id="two" text="2" btnClass="digit"/>
+                <Button id="three" text="3" btnClass="digit"/>
+                <Button id="add" text="+" btnClass="operator"/>
+                <Button id="decimal" text="." btnClass="operator"/> 
+                <Button id="zero" text="0" btnClass="digit"/>
+                <Button id="equals" text="=" btnClass="operator"/>
+                <Button id="subtract" text="-" btnClass="operator"/>
+                <Button id="clear" text="Clear" btnClass="clear"/>   
+            </div>
+        )
+    }
 }
 
 export default Calculator
